583d86b0d5d1c4dda12bca4443808f1e0402f6ba
gianluGit <gianlucarecchia1993@gmail.com>
2020-11-27 11:11:44 +0100

    react intro

---
+++ src/intro.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Intro extends Component {
+  render() {
+
+    return(
+      <h1>Hello World from React App</h1>
+    );
+  }
+}
+
+export default Intro;
